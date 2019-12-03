3d1ea948602563ee74a60e940b66209520e8056c
Matthew Blewitt <Matthew.Blewitt@hermes-europe.co.uk>
2019-12-03 11:40:55 +0000

    Add eslint + prettier + fix on autosave

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "prettier/vue",
+    "plugin:prettier/recommended"
+  ],
+  rules: {},
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+};
