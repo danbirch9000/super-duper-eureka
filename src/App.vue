9dbda286c9c30c6c639fcb8722f86bc06e95f793
Matthew Blewitt <Matthew.Blewitt@hermes-europe.co.uk>
2019-12-09 08:42:48 +0000

    WP-6838 Move app layout into sotrybook

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,12 @@
 <template>
-  <div id="app">
-    <SiteToolbar>
-      <SiteToolbarNav />
-    </SiteToolbar>
-    <SiteHeader v-slot="" />
-    <Breadcrumb :items="breadcrumb" />
-    <PageHeader>
-      Page header
-    </PageHeader>
-    <PageSubHeader>
-      Page sub heder
-    </PageSubHeader>
-    <PageWrapper>
-      <div class="container">
-        <DropdownMenu>
-          <template v-slot:menu-button="{ on }">
-            <BaseButton v-on="on">
-              Go to section <BaseIcon type="chevron-down" />
-            </BaseButton>
-          </template>
-          My list
-        </DropdownMenu>
-      </div>
-    </PageWrapper>
+  <div>
+    App
   </div>
 </template>
 
 <script>
-import SiteToolbar from "@/components/layout/SiteToolbar.vue";
-import SiteHeader from "@/components/layout/SiteHeader.vue";
-import SiteToolbarNav from "@/components/layout/SiteToolbarNav.vue";
-import PageWrapper from "@/components/layout/PageWrapper";
-import PageHeader from "@/components/layout/PageHeader";
-import PageSubHeader from "@/components/layout/PageSubHeader";
-import Breadcrumb from "@/components/nav/Breadcrumb";
-import DropdownMenu from "@/components/nav/DropdownMenu";
-import BaseButton from "@/components/BaseButton";
-import BaseIcon from "@/components/BaseIcon";
-
 export default {
-  name: "App",
-  components: {
-    SiteToolbar,
-    SiteToolbarNav,
-    SiteHeader,
-    PageWrapper,
-    PageHeader,
-    PageSubHeader,
-    Breadcrumb,
-    DropdownMenu,
-    BaseButton,
-    BaseIcon
-  },
-  data() {
-    return {
-      breadcrumb: [
-        {
-          text: "Home",
-          to: "#"
-        },
-        {
-          text: "Page title"
-        }
-      ]
-    };
-  }
+  name: "App"
 };
 </script>
 
---
+++ src/Layout.stories.js
@@ -0,0 +1,67 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from "@storybook/vue";
+import SiteToolbar from "@/components/layout/SiteToolbar.vue";
+import SiteHeader from "@/components/layout/SiteHeader.vue";
+import SiteToolbarNav from "@/components/layout/SiteToolbarNav.vue";
+import PageWrapper from "@/components/layout/PageWrapper";
+import PageHeader from "@/components/layout/PageHeader";
+import PageSubHeader from "@/components/layout/PageSubHeader";
+import Breadcrumb from "@/components/nav/Breadcrumb";
+import DropdownMenu from "@/components/nav/DropdownMenu";
+import BaseButton from "@/components/BaseButton";
+import BaseIcon from "@/components/BaseIcon";
+
+storiesOf("Layouts", module).add("Variations", () => ({
+  data() {
+    return {
+      breadcrumb: [
+        {
+          text: "Home",
+          to: "#"
+        },
+        {
+          text: "Page title"
+        }
+      ]
+    };
+  },
+  components: {
+    SiteToolbar,
+    SiteToolbarNav,
+    SiteHeader,
+    PageWrapper,
+    PageHeader,
+    PageSubHeader,
+    Breadcrumb,
+    DropdownMenu,
+    BaseButton,
+    BaseIcon
+  },
+  template: `
+    <div id="app">
+      <SiteToolbar>
+        <SiteToolbarNav />
+      </SiteToolbar>
+      <SiteHeader v-slot="" />
+      <Breadcrumb :items="breadcrumb" />
+      <PageHeader>
+        Page header
+      </PageHeader>
+      <PageSubHeader>
+        Page sub heder
+      </PageSubHeader>
+      <PageWrapper>
+        <div class="container">
+          <DropdownMenu>
+            <template v-slot:menu-button="{ on }">
+              <BaseButton v-on="on">
+                Go to section <BaseIcon type="chevron-down" />
+              </BaseButton>
+            </template>
+            My list
+          </DropdownMenu>
+        </div>
+      </PageWrapper>
+    </div>
+    `
+}));
