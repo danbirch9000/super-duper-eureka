42f35e763cc0521a58d2073cdbfb24da4f35e5f5
Matthew Blewitt <Matthew.Blewitt@hermes-europe.co.uk>
2019-12-09 09:40:55 +0000

    wp-6838 Update bundle

--- bundle.js
+++ bundle.js
@@ -1,3 +1,8 @@
-import MyButton from "./src/components/MyButton.vue";
+import AlertMessage from "./src/components/AlertMessage";
+import BaseButton from "./src/components/BaseButton";
+import BaseCard from "./src/components/BaseCard";
+import BaseIcon from "./src/components/BaseIcon";
+import BasePill from "./src/components/BasePill";
+import BaseTag from "./src/components/BaseTag";
 
-export { MyButton };
+export { AlertMessage, BaseButton, BaseCard, BaseIcon, BasePill, BaseTag };
