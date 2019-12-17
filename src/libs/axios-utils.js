558dcb20e6e5dbca7b9ea4f7aadfc934f930a2d6
Dan Birch <dan.birch@hermes-europe.co.uk>
2019-12-17 15:12:28 +0000

    Added axios library

---
+++ src/libs/axios-utils.js
@@ -0,0 +1,32 @@
+import {
+  START_USER_ACTIVITY,
+  SYSTEM_LOGOUT
+} from "wfp-vue-shared-components/src/libs/auth-utils";
+import { ENV_CONFIG } from "~/config/env-utils";
+
+export const setAxiosConfig = ({ env, $axios }) => {
+  $axios.interceptors.response.use(
+    response => handleHttpSuccess(response),
+    error => handleHttpError(error)
+  );
+  $axios.defaults.baseURL = ENV_CONFIG[env].baseURL;
+  $axios.defaults.timeout = 5000;
+  $axios.defaults.headers = {
+    Authorization: "Bearer: " + sessionStorage.getItem("access_token"),
+    location_id: "81",
+    location_type: "DEPOT"
+  };
+};
+
+const handleHttpError = error => {
+  START_USER_ACTIVITY();
+  if (error.response?.status === 401) {
+    SYSTEM_LOGOUT();
+  }
+  return Promise.reject(error);
+};
+
+const handleHttpSuccess = response => {
+  START_USER_ACTIVITY();
+  return response;
+};
